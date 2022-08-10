e1707b9f5938c34cf8162a0476e054d6733c81b9
le_woudar <rollandkev@yahoo.fr>
2022-08-10 10:08:06 +0200

    updated noxfile.py and ci

--- noxfile.py
+++ noxfile.py
@@ -6,27 +6,31 @@ nox.options.reuse_existing_virtualenvs = True
 @nox.session()
 def lint(session):
     """Performs pep8 and code formatting checks."""
+    session.install('poetry>=1.0.0,<2.0.0')
     session.run('poetry', 'install')
-    session.run('poetry', 'run', 'flake8')
-    session.run('poetry', 'run', 'black', '.', '--check')
+    session.run('flake8')
+    session.run('black', '.', '--check')
 
 
 @nox.session(tags=['security'])
 def bandit(session):
     """Performs code security checks."""
+    session.install('poetry>=1.0.0,<2.0.0')
     session.run('poetry', 'install')
-    session.run('poetry', 'run', 'bandit', 'quote.py')
+    session.run('bandit', 'quote.py')
 
 
 @nox.session(tags=['security'])
 def safety(session):
     """Checks vulnerabilities of the installed packages."""
+    session.install('poetry>=1.0.0,<2.0.0')
     session.run('poetry', 'install')
-    session.run('poetry', 'run', 'safety', 'check')
+    session.run('safety', 'check')
 
 
 @nox.session()
 def tests(session):
     """Run tests."""
+    session.install('poetry>=1.0.0,<2.0.0')
     session.run('poetry', 'install')
-    session.run('poetry', 'run', 'pytest')
+    session.run('pytest')
